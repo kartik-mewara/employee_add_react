59eca5fb616a5bc1694c41034cea8db6a8cedf31
kartik-mewara <48120409+kartik-mewara@users.noreply.github.com>
2020-11-13 21:41:06 +0530

    Create counter.jsx

---
+++ counter.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.value
+  };
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+        // onClick={()=.this.props.onDelete(this.props.ounter)}
+        // className="btn btn-secondary btn-sm"
+        >
+          employee
+        </button>
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+  formatCount() {
+    const { value } = this.state;
+    return value === 0 ? "ZERO" : value;
+  }
+}
+
+export default Counter;
